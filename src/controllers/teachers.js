3f5d61ab7e91bde72fde8fa17cb5177d3e52caa4
Ciro132000 <c.g_ciro@hotmail.com>
2022-09-01 09:31:03 -0500

    Solucionando modelos

--- src/controllers/teachers.js
+++ src/controllers/teachers.js
@@ -1,13 +1,21 @@
 // Impotaciones necesarias
 require('dotenv').config();
 const { QueryTypes } = require('sequelize');
-
 const fs =require('fs-extra')
 const { sequelize } = require('../config/connection')
 
+const {Estudiantes, Docentes, Secciones, Cursos} = require('../sequelize/models')
+
 // Funciones del controlador
-const mainFunction = (req, res) => {
-    res.send({data:"Funcionando"})
+const mainFunction = async (req, res) => {
+    const Data = await Docentes.findAll({
+        attributes: {exclude: ['idDocente']}
+    })
+    res.send({Data})
+}
+
+const registerTeacher = (req, res) => {
+    res.send({data:'hola'})
 }
 
 // Exportacion de funciones
